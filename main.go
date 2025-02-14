ce87e8962f5969bdf9167638f174ab4d508a4a8c
Joshua Nitschke <joshuanits@gmail.com>
2025-02-14 16:38:12 +1030

    feat: add tags to items

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+  "strings"
 
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
@@ -13,11 +14,19 @@ var docStyle = lipgloss.NewStyle().Margin(1, 2)
 
 type item struct {
 	title, desc string
+  tags []string
 }
 
 func (i item) Title() string       { return i.title }
-func (i item) Description() string { return i.desc }
-func (i item) FilterValue() string { return i.title }
+func (i item) Description() string { return i.desc + " " + i.FormatTags() }
+func (i item) FilterValue() string { return i.title + strings.Join(i.tags, " ") }
+func (i item) FormatTags() string {
+  if len(i.tags) == 0 {
+    return ""
+  }
+
+  return "(tags: " + strings.Join(i.tags, ", ") + ")"
+}
 
 type model struct {
 	list list.Model
@@ -49,26 +58,8 @@ func (m model) View() string {
 
 func main() {
 	items := []list.Item{
-		item{title: "Raspberry Pi’s", desc: "I have ’em all over my house"},
-		item{title: "Nutella", desc: "It's good on toast"},
-		item{title: "Bitter melon", desc: "It cools you down"},
-		item{title: "Nice socks", desc: "And by that I mean socks without holes"},
-		item{title: "Eight hours of sleep", desc: "I had this once"},
-		item{title: "Cats", desc: "Usually"},
-		item{title: "Plantasia, the album", desc: "My plants love it too"},
-		item{title: "Pour over coffee", desc: "It takes forever to make though"},
-		item{title: "VR", desc: "Virtual reality...what is there to say?"},
-		item{title: "Noguchi Lamps", desc: "Such pleasing organic forms"},
-		item{title: "Linux", desc: "Pretty much the best OS"},
-		item{title: "Business school", desc: "Just kidding"},
-		item{title: "Pottery", desc: "Wet clay is a great feeling"},
-		item{title: "Shampoo", desc: "Nothing like clean hair"},
-		item{title: "Table tennis", desc: "It’s surprisingly exhausting"},
-		item{title: "Milk crates", desc: "Great for packing in your extra stuff"},
-		item{title: "Afternoon tea", desc: "Especially the tea sandwich part"},
-		item{title: "Stickers", desc: "The thicker the vinyl the better"},
-		item{title: "20° Weather", desc: "Celsius, not Fahrenheit"},
-		item{title: "Warm light", desc: "Like around 2700 Kelvin"},
+    item{title: "Raspberry Pi’s", desc: "I have ’em all over my house", tags: []string{"computer", "electronics"}},
+    item{title: "Warm light", desc: "Like around 2700 Kelvin", tags: []string{"electronics"}},
 		item{title: "The vernal equinox", desc: "The autumnal equinox is pretty good too"},
 		item{title: "Gaffer’s tape", desc: "Basically sticky fabric"},
 		item{title: "Terrycloth", desc: "In other words, towel fabric"},
